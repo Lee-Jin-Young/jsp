f7f197ddb0b9f09b4b4b0a0508033b159a7f9eb1
LeeJinyoung <dlwlsdud618@naver.com>
2023-05-19 14:57:07 +0900

    html 형태로 표현

--- Step01Servlet/src/main/java/test/servlet/FortuneServlet.java
+++ Step01Servlet/src/main/java/test/servlet/FortuneServlet.java
@@ -2,6 +2,7 @@ package test.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,9 +16,35 @@ import javax.servlet.http.HttpServletResponse;
 public class FortuneServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//응답 인코딩 설정(utf-8 : 한글사용 가능)
+		resp.setCharacterEncoding("utf-8");
+		//응답 컨텐트 설정
+		resp.setContentType("text/html; charset=utf-8");
+
 		//클라이언트에게 문자열을 응답하는 객체의 참조값
 		PrintWriter pw = resp.getWriter();
-		pw.println("Today is Good!");
+		pw.println("<!DOCTYPE html>");
+		pw.println("<html>");
+		pw.println("<head>");
+		pw.println("<meta charset='utf-8'>");
+		pw.println("<title>오늘의 운세 페이지</title>");
+		pw.println("</head>");
+		pw.println("<body>");
+		pw.println("<div>");
+		Random rand = new Random();
+		
+		String[] fortunes = {"동쪽으로 가면 귀인을 만나요", 
+	            "오늘은 집에만 계세요", 
+	            "너무 멀리가지 마세요",
+	            "오늘은 뭘해도 되는 날이에요",
+	            "로또가 당첨되요"};
+
+		String fortune = fortunes[rand.nextInt(5)];
+		pw.println("<p>"+fortune+"</p>");
+		pw.println("</div>");
+		pw.println("</body>");
+		pw.println("</html>");
+		
 		pw.close();
 	}
 }
